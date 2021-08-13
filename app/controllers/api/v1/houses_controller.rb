13657a7ba44760797806c8dfeb2b40ae8f25c3b3
emmyn5600 <emmy6618@gmail.com>
2021-08-13 15:19:46 +0200

    Add fake data to check for api

--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -8,7 +8,27 @@ module Api
 
         def show 
             house = House.find(params[:id]);
-            render json: {status: 'SUCCESS', message:'Loaded House', data:houses}, status: :ok
+            render json: {status: 'SUCCESS', message:'Loaded House', data:house}, status: :ok
+        end
+
+        def create
+          user ||= user.find(house_params[:user_id])
+          if user             
+           house = user.houses.build(house_params)
+           if house.save
+            render json: {status: 'SUCCESS', message:'Houses created successfully', data:house}, status: :ok
+           else
+            render json: {status: 'ERROR', message:'Houses not created succesffully', data:house.errors}, status: :unprocessable_entry
+           end
+          else 
+            render json: {status: 404, message: 'User not found'}, status: 404
+          end
+        end
+
+        private 
+
+        def house_params
+          params.permit(:name, :description, :price, :image, :user_id);
         end
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,13 @@
+user = User.create(
+    name: 'emmy'
+)
+
 4.times do
     House.create({
         name: Faker::Name.first_name,
         description: Faker::Lorem.sentence,
         price: Faker::Lorem.sentence,
-        image: Faker::Lorem.sentence
+        image: Faker::Lorem.sentence,
+        user_id: user.id
     })
 end
