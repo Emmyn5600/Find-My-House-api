7ef18b9c22c29db46a15548bf412d04f59ec301f
emmyn5600 <emmy6618@gmail.com>
2021-08-13 12:08:37 +0200

    Add function to retreive house depend on the id

--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -5,6 +5,11 @@ module Api
             houses = House.all.order(Arel.sql('random()'));
             render json: {status: 'SUCCESS', message:'Loaded Houses', data:houses}, status: :ok
         end
+
+        def show 
+            house = House.find(params[:id]);
+            render json: {status: 'SUCCESS', message:'Loaded House', data:houses}, status: :ok
+        end
     end
   end
 end
