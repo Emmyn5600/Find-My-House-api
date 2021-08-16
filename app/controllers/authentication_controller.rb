c7a1af094c69f3dccb762d1660efd73e66d8686a
emmyn5600 <emmy6618@gmail.com>
2021-08-16 16:48:42 +0200

    Change the column name to is_admin in the user table

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,10 +1,10 @@
 class AuthenticationController < ApplicationController
   before_action :authorize_request, except: :login
-
+  
   def login
     @user = User.find_by_email(params[:email])
     if @user&.authenticate(params[:password])
-      token_data = { user_id: @user.id, name: @user.name, email: @user.email }
+      token_data = { user_id: @user.id, name: @user.name, email: @user.email, Is_admin: @user.is_admin }
       token = JsonWebToken.encode(token_data)
       time = Time.now + 24.hours.to_i
       render json: { token: token, exp: time.strftime('%m-%d-%Y %H:%M'),
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_135743) do
+ActiveRecord::Schema.define(version: 2021_08_16_144621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2021_08_16_135743) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
     t.string "password_digest"
-    t.boolean "Is_admin"
+    t.boolean "is_admin"
   end
 
 end
