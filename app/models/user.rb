2c972c1aebdcb9331425ea5c35726ab3a76f68f4
emmyn5600 <emmy6618@gmail.com>
2021-08-16 10:58:23 +0200

    Vaidate the email, name and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :name, presence: true, uniqueness: true
+  validates :password,
+            length: { minimum: 6 },
+            if: -> { new_record? || !password.nil? }
   has_many :houses
   has_many :rents
 end
