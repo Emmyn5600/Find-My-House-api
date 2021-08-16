97b98d62be46584be447dcc5d86523248b7dcf6e
emmyn5600 <emmy6618@gmail.com>
2021-08-16 15:58:56 +0200

    Add the is_admin column in the user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_080731) do
+ActiveRecord::Schema.define(version: 2021_08_16_135743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_08_16_080731) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
     t.string "password_digest"
+    t.boolean "Is_admin"
   end
 
 end
