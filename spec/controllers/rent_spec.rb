b6ec4b5252b54d4d529f14b68a4fa9ac3d2fb25c
emmyn5600 <emmy6618@gmail.com>
2021-08-20 10:56:19 +0200

    Add a rent if user_id exist

--- spec/controllers/rent_spec.rb
+++ spec/controllers/rent_spec.rb
@@ -9,4 +9,23 @@ describe 'the rent feature', type: :feature do
       rent = Rent.new(house_id: house.id, user_id: user.id)
       expect(rent.save).to be(true)
     end
+
+    # it 'should not add to favourites a course if no course_id is passed' do
+    #     user = User.new(name: 'emmy', email: 'emmy@gmail.com', password: 'password',
+    #         password_confirmation: 'password')
+    #         house = House.create(name: 'house1', price: '$400', description: 'this is the description of the house',
+    #             user_id: user.id)
+    #             rent = Rent.new(user_id: user.id)
+    #             expect(rent.save).to be(false)
+    #   end
+
+    it 'should add to rents a house if the user_id is valid' do
+        user = User.new(name: 'emmy', email: 'emmy@gmail.com', password: 'password',
+            password_confirmation: 'password')
+      house = House.create(name: 'house1', price: '$400', description: 'this is the description of the house',
+                             user_id: user.id)
+      rent = Rent.new(house_id: house.id, user_id: 1)
+      expect(rent.save).to be(true)
+      end
+    
 end
