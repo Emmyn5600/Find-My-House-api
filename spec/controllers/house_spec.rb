7575e909a33af2fea9a2d5675544009f5ec37c8f
emmyn5600 <emmy6618@gmail.com>
2021-08-20 10:36:42 +0200

    Add the tests to save a house if all information has passed

--- spec/controllers/house_spec.rb
+++ spec/controllers/house_spec.rb
@@ -6,4 +6,23 @@ describe 'houses managment validation', type: :feature do
     house = House.new(description: 'this is the description of the house', user_id: user.id, price: '$200')
     expect(house.save).to be(false)
   end
+
+  it 'should not save a house without the description' do
+    user = User.create(name: 'emmy', email: 'emmy@gmail.com', password: 'password')
+    house = House.new(name:"house1", user_id: user.id, price: '$200')
+    expect(house.save).to be(false)
+  end
+
+  it 'should not save a house without the price' do
+    user = User.create(name: 'emmy', email: 'emmy@gmail.com', password: 'password')
+    house = House.new(name: "house1", description: 'this is the description of the house', user_id: user.id)
+    expect(house.save).to be(false)
+  end
+
+  it 'should save a house if all the information is passed' do
+    user = User.create(name: 'emmy', email: 'emmy@gmail.com', password: 'password')
+    house = House.new(name: 'house1', description: 'this is the description of the house', user_id: user.id, price: '$200')
+    expect(house.name).to eql('house1')
+  end
+
 end
