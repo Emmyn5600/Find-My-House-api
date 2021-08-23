15a3637a861bb520660ca152b43b20060544394f
emmyn5600 <emmy6618@gmail.com>
2021-08-23 20:45:05 +0200

    Resolve conflict in the database

--- db/migrate/20210813081220_drop_table.rb
+++ db/migrate/20210813081220_drop_table.rb
@@ -1,7 +1,4 @@
 class DropTable < ActiveRecord::Migration[6.1]
   def change
-    drop_table :add_house_to_rents
-    drop_table :add_user_to_rents
-    drop_table :remove_house_to_rents
   end
 end
